e887400875be7fe06020d149a59c4263f44e5202
Adefuwa Oluwaponmile Hussein <34873655+Way4ward17@users.noreply.github.com>
2023-04-10 15:25:53 +0100

    Updates

--- src/main/java/com/example/digitalelectronic/HelloController.java
+++ src/main/java/com/example/digitalelectronic/HelloController.java
@@ -2,14 +2,23 @@ package com.example.digitalelectronic;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    private void chapterone(ActionEvent event) {
+    private void chapterone(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 642, 459);
+        Stage stage = new Stage();
+        stage.setTitle("Home");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
@@ -46,5 +55,45 @@ public class HelloController {
 
     @FXML
     private void chapterten(ActionEvent event) {
+    }
+
+    @FXML
+    private void chaptereleven(ActionEvent event) {
+    }
+
+    @FXML
+    private void chaptertwelve(ActionEvent event) {
+    }
+
+    @FXML
+    private void chapterthirteen(ActionEvent event) {
+    }
+
+    @FXML
+    private void chapterforteen(ActionEvent event) {
+    }
+
+    @FXML
+    private void chapterfiften(ActionEvent event) {
+    }
+
+    @FXML
+    private void chaptersixten(ActionEvent event) {
+    }
+
+    @FXML
+    private void chapterseventeen(ActionEvent event) {
+    }
+
+    @FXML
+    private void chaptereighten(ActionEvent event) {
+    }
+
+    @FXML
+    private void chapternineteen(ActionEvent event) {
+    }
+
+    @FXML
+    private void chaptertwenty(ActionEvent event) {
     }
 }
--- src/main/resources/fxml/FXMLController.java
+++ src/main/resources/fxml/FXMLController.java
@@ -25,44 +25,6 @@ public class FXMLController implements Initializable {
         // TODO
     }    
     
-    @FXML
-    private void chapterone(ActionEvent event) {
-    }
-
-    @FXML
-    private void chaptertwo(ActionEvent event) {
-    }
-
-    @FXML
-    private void chapterthree(ActionEvent event) {
-    }
-
-    @FXML
-    private void chapterfour(ActionEvent event) {
-    }
-
-    @FXML
-    private void chapterfive(ActionEvent event) {
-    }
-
-    @FXML
-    private void chaptersix(ActionEvent event) {
-    }
-
-    @FXML
-    private void chapterseven(ActionEvent event) {
-    }
-
-    @FXML
-    private void chaptereight(ActionEvent event) {
-    }
-
-    @FXML
-    private void chapternine(ActionEvent event) {
-    }
-
-    @FXML
-    private void chapterten(ActionEvent event) {
-    }
+   
 
 }
